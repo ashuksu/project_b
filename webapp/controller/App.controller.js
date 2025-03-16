d6121835755d591f360e3756a377a34d9cca9685
ashuksu <ashuksu@gmail.com>
2025-03-16 22:43:53 +0000

    create projet from teplate

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("app.project.b.projectb.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"appprojectb/project_b/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
